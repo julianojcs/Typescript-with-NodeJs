e696e13553d002f611db3d8db52a68e6ece7a130
Juliano Costa <apfjuliano@gmail.com>
2020-10-27 20:02:31 -0300

    Creating Knex Seeds - Lesson 15

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 
 app.use(routes)
+
+//Config static routes
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333, () => {
     console.log('Server up!')
